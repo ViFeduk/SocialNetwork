a61bcf05fb862edde4d264ad3e5dafa4723c31dc
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-06-15 17:36:15 +0300

    cal

---
+++ SocialNetwork/BLL/Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public int Sender_ID { get; set; }
+        public int Recipient_ID { get; set; }
+        public Message(int id, string content, int sender_id, int recipient_id)
+        {
+            Id = id;
+            Content = content;
+            Sender_ID = sender_id;
+            Recipient_ID = recipient_id;
+        }
+    }
+}
